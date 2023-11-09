4fd71585acd80076eaa9cd9ea8d382161e80ac80
OfficialAudite <dovydas.bumblauskas@aol.com>
2023-11-09 08:13:44 +0100

    Fix edit icon and add a round button instead

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -96,6 +96,10 @@
         showPopup = false;
     }
 
+    function printPage() {
+        window.print();
+    }
+
 </script>
 
 {#if showPopup}
@@ -136,11 +140,8 @@
     <div class="px-6 py-4 bg-gray-800 flex justify-between items-center">
         <h1 class="text-xl font-bold text-white">Time Estimation</h1>
         <div>
-            <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded">
+            <button class="print:hidden bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 rounded" on:click={() => printPage()}>
                 Export
-            </button>
-            <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={() => addNew()}>
-                + Add Item
             </button>
         </div>
     </div>
@@ -163,25 +164,39 @@
                 {/if}
                 {#each items as item}
                     <tr id={item.id}>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-400" contenteditable="true" 
-                            on:blur="{(event) => handleFieldChange(event, item.id, 'title')}">
+                        <td class="relative px-6 py-4 whitespace-nowrap text-sm text-gray-400">
+                        <div contenteditable="true" class="py-1" on:blur={(event) => handleFieldChange(event, item.id, 'title')}>
                             {item.title}
+                        </div>
+                        <object data="/edit.svg" type="image/svg+xml" class="print:hidden absolute top-1 right-1 w-4 h-4 cursor-pointer fill-current text-gray-500 hover:text-gray-300" aria-label="Edit Icon"></object>
                         </td>
-                        <td class="px-6 py-4 whitespace-normal text-sm text-gray-400 break-words max-w-s" contenteditable="true"
-                            on:blur="{(event) => handleFieldChange(event, item.id, 'description')}">
+                        <td class="relative px-6 py-4 whitespace-normal text-sm text-gray-400 break-words max-w-s">
+                        <div contenteditable="true" class="py-1" on:blur={(event) => handleFieldChange(event, item.id, 'description')}>
                             {item.description}
+                        </div>
+                        <object data="/edit.svg" type="image/svg+xml" class="print:hidden absolute top-1 right-1 w-4 h-4 cursor-pointer fill-current text-gray-500 hover:text-gray-300" aria-label="Edit Icon"></object>
                         </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-400" contenteditable="true"
-                            on:blur="{(event) => handleFieldChange(event, item.id, 'type')}">
+                        <td class="relative px-6 py-4 whitespace-nowrap text-sm text-gray-400">
+                        <div contenteditable="true" class="py-1" on:blur={(event) => handleFieldChange(event, item.id, 'type')}>
                             {item.type}
+                        </div>
+                        <object data="/edit.svg" type="image/svg+xml" class="print:hidden absolute top-1 right-1 w-4 h-4 cursor-pointer fill-current text-gray-500 hover:text-gray-300" aria-label="Edit Icon"></object>
                         </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-400" contenteditable="true"
+                        <td class="relative px-6 py-4 whitespace-nowrap text-sm text-gray-400">
+                        <div contenteditable="true" class="py-1"
                             on:focus={(event) => handleFocus(event, item)}
                             on:blur={(event) => handleBlur(event, item)}>
                             {item.displayTime}
+                        </div>
+                        <object data="/edit.svg" type="image/svg+xml" class="print:hidden absolute top-1 right-1 w-4 h-4 cursor-pointer fill-current text-gray-500 hover:text-gray-300" aria-label="Edit Icon"></object>
                         </td>
                     </tr>
                 {/each}
+                <tr>
+                    <td colspan="4" class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 text-center print:hidden">
+                        <button class="w-6 h-6 rounded-full bg-gray-400" on:click={() => addNew()}>+</button>
+                    </td>
+                </tr>
                 {#if items.length !== 0}
                     <tr class="bg-gray-700">
                         <td colspan="3" class="px-6 py-2 whitespace-nowrap text-sm text-gray-400 text-right font-bold">Summarized</td>
@@ -194,5 +209,5 @@
         </table>
     </div>
 </div>
-<p class="text-white text-[10px] mt-6 text-center">Made by <a href="https://github.com/OfficialAudite/" class=" text-blue-500 hover:text-blue-300 hover:underline">@officialaudite</a></p>
-<p class="text-white text-[10px] mb-6 text-center">For bug reports or feature requests, mail to <a href="mailto:info@audite.dev" class=" text-blue-500 hover:text-blue-300 hover:underline">info@audite.dev</a></p>
+<p class="text-white text-[10px] mt-6 text-center print:hidden">Made by <a href="https://github.com/OfficialAudite/" class=" text-blue-500 hover:text-blue-300 hover:underline">@officialaudite</a></p>
+<p class="text-white text-[10px] mb-6 text-center print:hidden">For bug reports or feature requests, mail to <a href="mailto:info@audite.dev" class=" text-blue-500 hover:text-blue-300 hover:underline">info@audite.dev</a></p>
