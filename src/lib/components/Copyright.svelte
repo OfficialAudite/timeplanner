2c30e63010e8dca2de2414687ca128ebc05e8237
OfficialAudite <dovydas.bumblauskas@aol.com>
2023-11-10 18:23:12 +0100

    Add disclaimer text

--- src/lib/components/Copyright.svelte
+++ src/lib/components/Copyright.svelte
@@ -1,2 +1,3 @@
+<p class="text-white text-[12px] my-4 text-center print:hidden">All data, including item details and the logo, is securely stored locally within your browser.</p>
 <p class="text-white text-[10px] mt-6 text-center print:hidden">Made by <a href="https://github.com/OfficialAudite/" class=" text-blue-500 hover:text-blue-300 hover:underline">@officialaudite</a></p>
-<p class="text-white text-[10px] mb-6 text-center print:hidden">For bug reports or feature requests, mail to <a href="mailto:info@audite.dev" class=" text-blue-500 hover:text-blue-300 hover:underline">info@audite.dev</a></p>
+<p class="text-white text-[10px] text-center print:hidden">For bug reports or feature requests, mail to <a href="mailto:info@audite.dev" class=" text-blue-500 hover:text-blue-300 hover:underline">info@audite.dev</a></p>
