27d0cd58ec9d297adc04fe67e6b42797de6fc9c6
OfficialAudite <dovydas.bumblauskas@aol.com>
2023-11-10 19:38:54 +0100

    Hotfix for lang

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,5 @@
 <script>
-    import "../app.css";
-    import { onMount } from 'svelte';
-    import { browser } from '$app/environment';
-    import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
-
-    register('en', () => import('../locales/en.json'));
-    register('sv', () => import('../locales/sv.json'));
-
-    let initialLocale = 'en'; // default locale
-
-    onMount(() => {
-        if (browser) {
-            const savedLanguage = localStorage.getItem('locale');
-            initialLocale = savedLanguage || getLocaleFromNavigator();
-        }
-
-        const supportedLanguages = ['en', 'sv'];
-        initialLocale = supportedLanguages.includes(initialLocale) ? initialLocale : 'en';
-
-        init({
-            fallbackLocale: 'en',
-            initialLocale: initialLocale,
-        });
-    });
+  import "../app.css";
 </script>
 
 <div class="max-w-7xl mx-auto my-8">
