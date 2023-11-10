3b6674207078a0144800102a6320f203dca9ee66
OfficialAudite <dovydas.bumblauskas@aol.com>
2023-11-11 00:09:01 +0100

    Translation button

--- src/lib/components/Copyright.svelte
+++ src/lib/components/Copyright.svelte
@@ -5,3 +5,4 @@
 <p class="text-white text-[12px] my-4 text-center print:hidden max-w-lg mx-auto">{$t('disclaimer')}</p>
 <p class="text-white text-[10px] mt-6 text-center print:hidden">{$t('copyright1')} <a href="https://github.com/OfficialAudite/" class=" text-blue-500 hover:text-blue-300 hover:underline">@officialaudite</a></p>
 <p class="text-white text-[10px] text-center print:hidden">{$t('copyright2')} <a href="mailto:info@audite.dev" class=" text-blue-500 hover:text-blue-300 hover:underline">info@audite.dev</a></p>
+<p class="text-white text-[10px] text-center print:hidden"><a href="https://poeditor.com/join/project/qdI4O44nJM">Translate</a></p>
