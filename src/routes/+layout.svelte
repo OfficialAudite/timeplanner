d83ace3d7bec7a74b2ef4e780c8aa3e2c8b7608f
OfficialAudite <dovydas.bumblauskas@aol.com>
2023-11-10 19:47:26 +0100

    Hotfix for lang

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script>
   import "../app.css";
+  import { isLoading } from "svelte-i18n";
 </script>
 
 <div class="max-w-7xl mx-auto my-8">
-    <slot />
+    {#if $isLoading}
+    <div class="flex justify-center items-center">
+        <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
+    </div>
+    {:else}
+        <slot />
+    {/if}
 </div>
 
 <style>
